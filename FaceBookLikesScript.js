775d561d0c9b92b6a8dbe4905163befac6c33869
spiridonov-oa <spiridonov.oa@gmail.com>
2016-09-26 14:28:55 +0300

    Improve script with autoscroll to bottom

--- FaceBookLikesScript.js
+++ FaceBookLikesScript.js
@@ -1,2 +1,20 @@
-var likeBtns = [...document.querySelectorAll('.UFILikeLink:not(.UFILinkBright)')];
-likeBtns.forEach(function(el,i) {setTimeout(function () {el.click();console.log( (i + 1) + ' - liked ;) of ' + likeBtns.length)}, i*2000) })
+function likeAll () {
+	setTimeout(()=>{
+		window.scrollTo(0,10000000);
+		console.log('scrolled');
+		var likeBtns = [...document.querySelectorAll('.UFILikeLink:not(.UFILinkBright)')];
+		if (!likeBtns.length) {
+			likeAll();
+		}
+		likeBtns.forEach(function(el,i) {
+		setTimeout(() => {
+			el.click();
+			console.log( (i + 1) + ' - liked ;) of ' + likeBtns.length);
+			if (i === likeBtns.length - 1) {
+				likeAll();
+			}
+		}, i*2000) });
+	}, 1000);
+}
+	
+likeAll();
